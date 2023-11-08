4e8fdabff13a3df3058aaa04c32a631994b0ce04
cekdarcpr <cekooo12@gmail.com>
2023-11-08 22:42:21 +0300

    WorkOut

--- WorkOut01/Program.cs
+++ WorkOut01/Program.cs
@@ -2,73 +2,109 @@
 
 namespace WorkOut01
 {
+    internal class AlanHsplm
+    {
+        public int Kenar1 { get; set; }
+        public int Kenar2 { get; set; }
+
+        public double YariCap { get; set; }
+
+        public double Cember
+        {
+            get
+            {
+
+                return Math.PI * Math.Pow(YariCap, 2);
+            }
+        }
+
+    }
+
+    internal class DikdörtgenHsplm : AlanHsplm
+    {
+        public int dikdörtgen
+        {
+            get
+            {
+                return Kenar1 * Kenar2;
+            }
+        }
+    }
+
+    internal class KareHsplm : AlanHsplm
+    {
+        public int kare
+        {
+            get
+            {
+                return Kenar1 * Kenar2;
+            }
+        }
+    }
+
     internal class Program
     {
-        internal class Staff
-        {
-            
-            public int kenar { get; set; }
-            public int kenar2 { get; set; }
-            public double yarıcapı { get; set; }
-
-            public  double cember
-            {
-                get
-                {
-                    return  Math.PI * Math.Pow(yarıcapı,2) ;
-                }
-            }
-        }
-
-        internal class Worker : Staff
-        {
-            public int dikdortgen
-            {
-                get
-                {
-                    return kenar * kenar2;
-                }
-            }
-        }
-
-        internal class Manager : Staff
-        {
-            public int kare
-            {
-                get
-                {
-                    return kenar * kenar2 ;
-                }
-            }
-        }
-
-    
-
         static void Main(string[] args)
         {
 
 
-            Staff cember = new Staff();
-            cember.yarıcapı = 5 ;
-          
 
-            Worker dikdortgen = new Worker();
-            dikdortgen.kenar = 8;
-            dikdortgen.kenar2 = 11;
-           
+            while (true)
+            {
+                Console.WriteLine("Hangi Alan Hesabı İstediğinizi Seçin :");
+                int secim = Convert.ToInt32(Console.ReadLine());
 
-            Manager kare = new Manager();
-            kare.kenar = 10;
-            kare.kenar2 = 10;
+                if (secim == 1)
+                {
+                    AlanHsplm alanHsplm = new AlanHsplm();
+                    Console.WriteLine("Lütfen Yarıçapı giriniz : ");
+                    alanHsplm.YariCap = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Cemberin Yarıçapı {alanHsplm.YariCap} Cemberin Alanı {alanHsplm.Cember}");
+                }
+                else if (secim == 2)
+                {
+                    DikdörtgenHsplm dikdörtgenHsplm = new DikdörtgenHsplm();
+                    Console.WriteLine("1. Kenarı giriniz : ");
+                    dikdörtgenHsplm.Kenar1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("2. Kenarı giriniz : ");
+                    dikdörtgenHsplm.Kenar2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($" Dikdörtgenin Alanı : {dikdörtgenHsplm.dikdörtgen}");
+                }
+                else if (secim == 3)
+                {
+                    KareHsplm kareHsplm = new KareHsplm();
+                    Console.WriteLine("1. Kenarı giriniz : ");
+                    kareHsplm.Kenar1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("2. Kenarı giriniz : ");
+                    kareHsplm.Kenar2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($" Kare Alanı : {kareHsplm.kare}");
 
-           
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı seçim...");
+                }
+            }
 
-            Console.WriteLine($"Cemberin Yarıçapı {cember.yarıcapı} Cemberin Alanı {cember.cember}");
-            Console.WriteLine(dikdortgen.dikdortgen);
-            Console.WriteLine(kare.kare);
-        
 
+            //Console.WriteLine("Lütfen Yarıçapı giriniz : ");
+            //alanHsplm.YariCap = Convert.ToDouble(Console.ReadLine());
 
+            //DikdörtgenHsplm dikdörtgenHsplm = new DikdörtgenHsplm();
+            //Console.WriteLine("1. Kenarı giriniz : ");
+            //dikdörtgenHsplm.Kenar1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2. Kenarı giriniz : ");
+            //dikdörtgenHsplm.Kenar2 = Convert.ToInt32(Console.ReadLine());
+
+            //KareHsplm kareHsplm = new KareHsplm();
+            //Console.WriteLine("1. Kenarı giriniz : ");
+            //kareHsplm.Kenar1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2. Kenarı giriniz : ");
+            //kareHsplm.Kenar2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Cemberin Yarıçapı {alanHsplm.YariCap} Cemberin Alanı {alanHsplm.Cember}");
+
+            Console.ReadKey();
         }
     }
 }
